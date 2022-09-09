a76bafaffe79b6bc0bd854df42ebc30a6c03a228
tipsymelony <tipsymelony@gmail.com>
2022-09-08 20:01:41 -0700

    Add Task 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,16 +3,15 @@
 
 
 /**
- *
- *  * main - The purpose of this file is ti out
- *
- *   * the string: with proper grammar, but the outcome is a piece of art
- *
- *    * followed by a new line, using printf and not puts.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
+    *
+    * main - The purpose of this file is ti out
+    *
+    * the string: with proper grammar, but the outcome is a piece of art
+    *
+    * followed by a new line, using printf and not puts.
+    *
+    * Return: Always 0 (Success)
+    */
 
 int main(void)
 
