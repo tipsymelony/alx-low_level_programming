a6b1e555a4bbd2a87ab686e6ef4ae853cb3a9975
tipsymelony <tipsymelony@gmail.com>
2022-09-08 20:36:36 -0700

    Add 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@
     */
 
 int main(void)
-
 {
 
 		printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -18,5 +17,4 @@ int main(void)
          	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 		printf("Size of a float: %lu byte(s)\n", sizeof(float));
 		return (0);
-
 }
