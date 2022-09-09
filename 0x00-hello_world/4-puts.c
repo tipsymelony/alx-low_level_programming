7e6304122104c8a2c5b921f2bf8a13e43e86d7ac
tipsymelony <tipsymelony@gmail.com>
2022-09-08 19:51:23 -0700

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Write a C program that prints exactly
- *
- *   * "Programming is like building a multilingual puzzle
- *
- *    * followed by a new line.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
+    * main - Write a C program that prints exactly
+    *
+    * "Programming is like building a multilingual puzzle
+    *
+    * followed by a new line.
+    *
+    * Return: Always 0 (Success)
+    *
+    * */
 
 int main(void)
 
