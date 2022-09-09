92d881d083d824fb92591c05ac2c4d45adfe818d
tipsymelony <tipsymelony@gmail.com>
2022-09-08 19:38:56 -0700

    Add Task 1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Write a C program that prints exactly
+ *
+ *   * "Programming is like building a multilingual puzzle
+ *
+ *    * followed by a new line.
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - The purpose of this file is ti out
+ *
+ *   * the string: with proper grammar, but the outcome is a piece of art
+ *
+ *    * followed by a new line, using printf and not puts.
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+			return (0);
+
+}
