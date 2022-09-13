9b00c02c01c736b2c85c7c502f901640f407b27c
tipsymelony <tipsymelony@gmail.com>
2022-09-13 13:44:49 -0700

    Add _putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and error is set appropraitely
+ */
+int _purchar(char c)
+{
+	return (write(1, &c, 1));
+}
