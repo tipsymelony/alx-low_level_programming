0a920b5a70d87c71d831878b8f0c3623c7bec1b6
tipsymelony <tipsymelony@gmail.com>
2022-09-13 13:25:46 -0700

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Lanzador
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
